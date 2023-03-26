7a6cc7270dbac4245940f3441f098aaf2f67a7b7
Jhonatan Nuss <contato@jhonatanuss.dev>
2023-03-26 20:29:36 -0300

    SQL

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,13 +1,30 @@
-CREATE TABLE TB_USER (
-                            id BIGINT PRIMARY KEY AUTO_INCREMENT,
-                            NOME VARCHAR(100) NOT NULL,
-                            SENHA VARCHAR(100) NOT NULL,
-                            EMAIL VARCHAR(100) UNIQUE NOT NULL,
-                            TELEFONE INT(12),
-                            PACOTE_MENSAL BOOLEAN,
-                            NASCIMENTO DATE NOT NULL
-);
+CREATE TABLE TB_USER (ID BIGINT  NOT NULL AUTO_INCREMENT,
+                      FIRSTNAME VARCHAR(100) NOT NULL,
+                      LASTNAME VARCHAR(100) NOT NULL,
+                      PASSWORD VARCHAR(100) NOT NULL,
+                      BIRTH DATETIME(6) NOT NULL,
+                      PHONE INTEGER,
+                      EMAIL VARCHAR(100) NOT NULL,
+                      PACKAGE_MONTHLY BIT,
+                      ROLE VARCHAR(255), PRIMARY KEY (ID)) ENGINE=INNODB;
 
+INSERT INTO TB_USER (FIRSTNAME, LASTNAME, PASSWORD, BIRTH, PHONE, EMAIL, PACKAGE_MONTHLY, ROLE)
+VALUES ('Admin', 'lastname', '$2a$10$Yb.6O1lhvE95OoA8mHnV7.tu1SZTZdnV7oZ28dYlP7TifvmhkwgGi', '2000-01-01 00:00:00', NULL, 'admin@exemple.com', FALSE, 'USER');
+-------------------------------------------------------------------------------------------------------------
+CREATE TABLE TB_SERVICE_PROVIED (ID BIGINT NOT NULL AUTO_INCREMENT,
+                                 DATA DATETIME(6) NOT NULL, VALUE INTEGER,
+                                 USER_ID BIGINT, PRIMARY KEY (ID)) ENGINE=INNODB;
 
-INSERT INTO TB_USER (NOME, SENHA, EMAIL, TELEFONE, PACOTE_MENSAL, NASCIMENTO)
-VALUES ('Nome do usuário', 'senha123', 'usuario@exemplo.com', 1234567890, 'true', '2000-01-01');
+-------------------------------------------------------------------------------------------------------------
+
+CREATE TABLE TB_APPOINMENT (ID BIGINT NOT NULL AUTO_INCREMENT,
+                            DATE_TIME DATETIME(6) NOT NULL,
+                            USER_ID BIGINT, PRIMARY KEY (ID)) ENGINE=INNODB;
+
+-------------------------------------------------------------------------------------------------------------
+CREATE TABLE SERVICE_PROVIED_SERVICE_TYPE (SERVICE_PROVIED_ID BIGINT NOT NULL,
+                                           SERVICE_TYPE INTEGER NOT NULL) ENGINE=INNODB;
+
+CREATE TABLE APPOINTMENT_SERVICE_TYPE (APPOINTMENT_ID BIGINT NOT NULL,
+                                       SERVICE_TYPE INTEGER NOT NULL) ENGINE=INNODB;
+
