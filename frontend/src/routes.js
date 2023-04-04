b06a0f9b3decb5f162a93b64e00dd4291b342623
Jhonatan Nuss <contato@jhonatanuss.dev>
2023-04-04 20:29:50 -0300

    Adicionando o front do projeto, usando dashboard do MUI, lista de usuários já ajustada, listando todos os usuarios

---
+++ frontend/src/routes.js
@@ -0,0 +1,40 @@
+import { Navigate, useRoutes } from 'react-router-dom';
+// layouts
+import DashboardLayout from './layouts/dashboard';
+import SimpleLayout from './layouts/simple';
+//
+import UserPage from './pages/UserPage';
+import LoginPage from './pages/Login';
+import ProductsPage from './pages/ProductsPage';
+import DashboardAppPage from './pages/DashboardAppPage';
+
+// ----------------------------------------------------------------------
+
+export default function Router() {
+  const routes = useRoutes([
+    {
+      path: '/dashboard',
+      element: <DashboardLayout />,
+      children: [
+        { element: <Navigate to="/dashboard/app" />, index: true },
+        { path: 'app', element: <DashboardAppPage /> },
+        { path: 'user', element: <UserPage /> },
+        { path: 'products', element: <ProductsPage /> },
+      ],
+    },
+    {
+      path: 'login',
+      element: <LoginPage />,
+    },
+    {
+      element: <SimpleLayout />,
+      children: [
+        { element: <Navigate to="/dashboard/app" />, index: true },
+        { path: '*', element: <Navigate to="/404" /> },
+      ],
+    },
+
+  ]);
+
+  return routes;
+}
